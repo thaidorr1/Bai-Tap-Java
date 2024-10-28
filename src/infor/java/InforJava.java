c478c34eb14f13a198675c961422068321df4612
doem5 <doem5@MSI>
2024-10-28 23:41:19 +0700

    Code infor by Java

---
+++ src/infor/java/InforJava.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package infor.java;
+
+/**
+ *
+ * @author doem5
+ */
+public class InforJava {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("thai thanh cong");
+        System.out.println("CMU TPM15");
+        System.out.println("29211141385");
+    }
+    
+}
